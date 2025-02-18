fac61b3fe7b160337132d91167d1bedf9abe7d0d
AlexPugaciov <152472989+AlexPugaciov@users.noreply.github.com>
2025-02-18 07:31:29 +0200

    Update main+assync.py

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
-import requests
+import aiohttp
+import asyncio
+from aiohttp import TCPConnector
 from bs4 import BeautifulSoup
 
-def news_parser(page: int, sport: str):
+async def fetch_page(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def fetch_article(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def news_parser(page: int, sport: str):
     url = "https://www.*****.ru/news/"
     url_temp = url
-    full_news = list()
-    links_list = list()
+    full_news = []
+    links_list = []
     sport_len = len(sport) + 1
 
-    for num in range(2, page + 2):
+    
+    connector = TCPConnector(ssl=False)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        tasks = []
 
-        response = requests.get(url_temp)
-        html_content = response.content
-        soup = BeautifulSoup(html_content, 'html.parser')
-        articles = soup.find_all('div', class_='articles-item')
+        
+        for num in range(2, page + 2):
+            tasks.append(fetch_page(session, url_temp))
 
-        url_temp = url + f'page{num}/'
+            url_temp = url + f'page{num}/'
 
-        for article in articles:
-            link = article.find('a')['href']
-            full_url = 'https://www.****.ru' + link
+   
+        pages_content = await asyncio.gather(*tasks)
 
-            if link[1:sport_len] == sport:
-                links_list.append(full_url)
+      
+        for content in pages_content:
+            soup = BeautifulSoup(content, 'html.parser')
+            articles = soup.find_all('div', class_='articles-item')
 
+            for article in articles:
+                link = article.find('a')['href']
+                full_url = 'https://www.*****.ru' + link
 
-    for link_elem in links_list:
+                if link[1:sport_len] == sport:
+                    links_list.append(full_url)
 
-        resp_elem = requests.get(link_elem)
-        elem_content = resp_elem.content
-        soup_el = BeautifulSoup(elem_content, 'html.parser')
-        article_text_div = soup_el.find('div', class_='article-text clearfix')
-       
-      if article_text_div:
-            article_text = article_text_div.get_text(strip=True)
-            if article_text :
-                full_news.append((link_elem,article_text[8:]))
+    
+        article_tasks = []
+        for link_elem in links_list:
+            article_tasks.append(fetch_article(session, link_elem))
+
+        article_contents = await asyncio.gather(*article_tasks)
+
+        
+        for content, link_elem in zip(article_contents, links_list):
+            soup_el = BeautifulSoup(content, 'html.parser')
+            article_text_div = soup_el.find('div', class_='article-text clearfix')
+
+            if article_text_div:
+                article_text = article_text_div.get_text(strip=True)
+                if article_text:
+                    full_news.append((link_elem, article_text[8:]))
+
     return full_news
 
 
-my_news = news_parser(page=10,
-                      sport= 'hock')
-for news in my_news:
-    print(news[0])
-    print(news[1])
+async def main():
+    my_news = await news_parser(page=10, sport='hock')
+    for news in my_news:
+        print(news[0])
+        print(news[1])
+
+
+asyncio.run(main())
